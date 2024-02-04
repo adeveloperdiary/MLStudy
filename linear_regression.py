5ffbcbd5e5b853031d8bf1110da3827d51b81df8
Abhisek Jana <adeveloperdiary@gmail.com>
2024-02-04 02:23:14 -0500

    Linear regression

--- linear_regression.py
+++ linear_regression.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 class LinearRegression:
-    def __init__(self, lr: float = 1e-3, epochs: int = 100, debug: bool = False):
+    def __init__(self, lr: float = 1e-3, epochs: int = 2000, debug: bool = False):
         self.lr = lr
         self.epochs = epochs
         self.debug = debug
@@ -13,11 +13,23 @@ class LinearRegression:
         # Add bias with the data
         X = np.append(bias, X, axis=1)
 
-        w = np.zeros((X.shape[1], 1))
+        self.w = np.zeros((X.shape[1], 1))
 
         for epoch in range(self.epochs):
-            y_hat=np.dot(X,w)
-            
+            y_hat = np.dot(X, self.w)
+            cost = (1 / X.shape[0]) * (np.sum(np.square(y_hat - y)))
+
+            if epoch % 100 == 0:
+                print(f"Cost after {epoch}: {cost}")
+
+            self.w -= self.lr * (2 / X.shape[0]) * (np.dot(X.T, (y_hat - y)))
+
+    def predict(self, X, y):
+        bias = np.zeros((X.shape[0], 1))
+        X = np.append(bias, X, axis=1)
+        y_hat = np.dot(X, self.w)
+        cost=(1/X.shape[0])*(np.sum(np.square(y_hat-y)))
+        print(f"Test Cost is: {cost}")
         
 
 
@@ -29,7 +41,9 @@ if __name__ == "__main__":
     X = np.append(X1, X2, axis=1)
 
     # Generate Y using X
-    y = X1*3+X2*2+np.random.normal(0, 1)
+    y = X1 * 3 + X2 * 2 + np.random.normal(0, 1)
 
     model = LinearRegression(debug=True)
-    model.fit(X, y)
+    model.fit(X[:450,:], y[:450,:])
+    
+    model.predict(X[:-50,:], y[:-50,:])
